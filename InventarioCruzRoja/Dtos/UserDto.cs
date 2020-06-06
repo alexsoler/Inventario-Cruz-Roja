abcc6813e0c47bd6de3d6fe21943bb2a03adc6af
alexsoler <alexgeovanys@gmail.com>
2020-06-06 14:41:29 -0600

    Cambiar nombre y agregar propiedades objeto dto

--- InventarioCruzRoja/Dtos/UserDto.cs
+++ InventarioCruzRoja/Dtos/UserDto.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 
 namespace InventarioCruzRoja.Dtos
 {
-    public class UserRegisterDto
+    public class UserDto
     {
+        public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public List<string> Roles { get; set; }
     }
 }
