e06d4247bb4eb5753e1c1bf4fcaa9a527e116a16
alexsoler <alexgeovanys@gmail.com>
2020-06-06 14:43:16 -0600

    Inicializar propiedad con string vacio

--- InventarioCruzRoja/Models/ServiceResponse.cs
+++ InventarioCruzRoja/Models/ServiceResponse.cs
@@ -9,6 +9,6 @@ namespace InventarioCruzRoja.Models
     {
         public T Data { get; set; }
         public bool Success { get; set; } = true;
-        public string Message { get; set; }
+        public string Message { get; set; } = string.Empty;
     }
 }
