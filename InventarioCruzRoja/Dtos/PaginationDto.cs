43c3d3b2e0b6a8702e951739232aa92b05326e75
alexsoler <alexgeovanys@gmail.com>
2020-06-06 14:46:51 -0600

    Agregar clase para enviar registros paginados

---
+++ InventarioCruzRoja/Dtos/PaginationDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventarioCruzRoja.Dtos
+{
+    public class PaginationDto<T> where T : class
+    {
+        public IEnumerable<T> Items { get; set; }
+        public int Total { get; set; }
+    }
+}
