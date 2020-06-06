339a6de2c062331c479883de3cf38d570710f966
alexsoler <alexgeovanys@gmail.com>
2020-06-06 14:44:10 -0600

    Agregar entidad UserTableDto

---
+++ InventarioCruzRoja/Dtos/UserTableDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventarioCruzRoja.Dtos
+{
+    public class UserTableDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public List<string> Roles { get; set; }
+    }
+}
