970fdb12331457df4348a3b1b495eb50cb025206
Alex Soler <alex.soler@farsiman.com>
2021-08-22 20:14:29 -0600

    Agregar servicio de proveedores

---
+++ InventarioCruzRoja/clientapp/src/services/proveedores.service.js
@@ -0,0 +1,45 @@
+import axios from '../plugins/http'
+
+class ProveedoresService {
+    async getAll () {
+      try {
+        const response = await axios.get('/api/proveedores')
+
+        return response
+      } catch (error) {
+        return error
+      }
+    }
+
+    async create (proveedor) {
+      try {
+        const response = await axios.post('/api/proveedores', proveedor)
+
+        return response
+      } catch (error) {
+        return error
+      }
+    }
+
+    async edit (id, proveedor) {
+      try {
+        const response = await axios.put(`/api/proveedores/${id}`, proveedor)
+
+        return response
+      } catch (error) {
+        return error
+      }
+    }
+
+    async delete (id) {
+      try {
+        const response = await axios.delete(`/api/proveedores/${id}`)
+
+        return response
+      } catch (error) {
+        return error
+      }
+    }
+}
+
+export default new ProveedoresService()
