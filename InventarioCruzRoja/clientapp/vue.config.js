fb06ff8c1072332ec047c1a793e2a22dc1ac3e2d
Alex Soler <alex.soler@farsiman.com>
2021-08-22 20:14:02 -0600

    Instalar paquete vue-tel-input-vuetify

--- InventarioCruzRoja/clientapp/vue.config.js
+++ InventarioCruzRoja/clientapp/vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     disableHostCheck: true,
   },
 
-  transpileDependencies: ['vuetify'],
+  transpileDependencies: ['vuetify', 'vue-tel-input-vuetify'],
 
   pluginOptions: {
     i18n: {
