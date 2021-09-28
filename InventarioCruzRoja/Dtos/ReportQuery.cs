c393b2ad3013ac591e7881370a45061a30a86d35
Alex Soler <alex.soler@farsiman.com>
2021-09-27 21:43:27 -0600

    Generar reporte de ingreso 🤩

---
+++ InventarioCruzRoja/Dtos/ReportQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventarioCruzRoja.Dtos
+{
+    public class ReportQuery
+    {
+        public string Format { get; set; }
+        public bool Inline { get; set; }
+        public string Parameter { get; set; }
+    }
+}
