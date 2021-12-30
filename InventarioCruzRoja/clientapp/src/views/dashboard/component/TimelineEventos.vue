673a84829b6c199cc49c510ef4636362d421f013
Alex Soler <alex.soler@farsiman.com>
2021-12-29 18:26:17 -0600

    Agregar iconos y color por evento 🙉

--- InventarioCruzRoja/clientapp/src/views/dashboard/component/TimelineEventos.vue
+++ InventarioCruzRoja/clientapp/src/views/dashboard/component/TimelineEventos.vue
@@ -15,7 +15,7 @@
         >
           <template v-slot:icon>
             <v-icon>
-                mdi-timeline
+              mdi-timeline
             </v-icon>
           </template>
         </v-timeline-item>
@@ -26,9 +26,13 @@
             v-for="(event, i) in eventos"
             :key="i"
             class="mb-4"
-            color="pink"
-            small
+            :color="getColor(event.descripcion)"
           >
+            <template v-slot:icon>
+              <v-icon>
+                {{ getIcon(event.descripcion) }}
+              </v-icon>
+            </template>
             <v-row justify="space-between">
               <v-col
                 cols="7"
@@ -74,6 +78,36 @@
     methods: {
       close () {
         this.$emit('close')
+      },
+      getIcon (text) {
+        if (text.includes('agrego')) {
+          return 'mdi-clipboard-plus'
+        } else if (text.includes('anulo')) {
+          return 'mdi-clipboard-remove'
+        } else if (text.includes('ingreso')) {
+          return 'mdi-clipboard-arrow-down'
+        } else if (text.includes('egreso')) {
+          return 'mdi-clipboard-arrow-up'
+        } else if (text.includes('edito')) {
+          return 'mdi-clipboard-edit'
+        }
+
+        return 'mdi-clipboard'
+      },
+      getColor (text) {
+        if (text.includes('agrego')) {
+          return 'green'
+        } else if (text.includes('anulo')) {
+          return 'red'
+        } else if (text.includes('ingreso')) {
+          return 'blue'
+        } else if (text.includes('egreso')) {
+          return 'yellow'
+        } else if (text.includes('edito')) {
+          return 'purple'
+        }
+
+        return 'blue-grey'
       },
     },
   }
